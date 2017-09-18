7ee499490f7c5482ba62cb541e4259be1fd1a0ec
marc-antoine <papibenjie@gmail.com>
2017-09-18 11:44:48 -0400

    test

--- dofusteam/settings.py
+++ dofusteam/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dofusteam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["sandbox/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sandbox/views.py
+++ sandbox/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('sandbox/index.html')
+    context = {
+        "list": [1,2,3]
+    }
+    return HttpResponse(template.render(context, request))
