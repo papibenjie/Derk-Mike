f25db3acf832f5805bbf6f70d1778791ecef2afe
marc-antoine <papibenjie@gmail.com>
2017-09-20 19:34:05 -0400

    move api call to backend

--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+import requests
+import json
+import random
+
 
 def index(request):
     template = loader.get_template('portal/index.html')
+
+    uri = "https://pokeapi.co/api/v2/pokemon/"+str(random.randint(1,700))
+    response = requests.get(uri)
+    json_data = json.loads(response.text)
+    a = {k: v for k, v in json_data["sprites"].items() if v}
+    pos, uri = random.choice(list(a.items()))
     context = {
-        "list": [1,2,3]
+        "rand_poke_uri": uri
     }
     return HttpResponse(template.render(context, request))
