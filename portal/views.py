5790c9a1acd4f7350e91f62d5687c8fb751aec2e
marc-antoine <papibenjie@gmail.com>
2017-09-22 17:40:53 -0400

    test

--- portal/views.py
+++ portal/views.py
@@ -1,17 +1,15 @@
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import HttpResponse
 from django.template import loader
 import random
-import requests
-import json
-import random
-import os
+
 
 def index(request):
 
     template = loader.get_template('portal/index.html')
-    url = 'global/images/mike_faces/mike-face-'+str(random.randint(1,7))+'.png'
+    url = 'global/images/mike_faces/mike-face-' + str(random.randint(1, 7)) + \
+        '.png'
+
     context = {
-        "rand_mike":url
+        "rand_mike": url
     }
     return HttpResponse(template.render(context, request))
