da976e8bfabafce5a057dfa2c9e5b075616d9e36
marc-antoine <papibenjie@gmail.com>
2017-09-23 16:49:19 -0400

    gud

--- derkmike/settings.py
+++ derkmike/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'sandbox',
     'portal',
+    'derkmike',
     # Disable Django's own staticfiles handling in favour of WhiteNoise, for
     # greater consistency between gunicorn and `./manage.py runserver`. See:
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
